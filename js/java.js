224262e41f8b7b3d977f6310f5593b0b41a40679
jeralynmedrano <62765852+jeralynmedrano@users.noreply.github.com>
2020-04-30 01:04:02 -0700

    added images, nav, scroll, text

---
+++ js/java.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+  $("#shape1").click(function() {
+                   $(this).fadeOut(500);
+   
+    // use addClass with
+    // $(this).addClass("clicked");
+  });
+
+  $("#foam").click(function() {
+    $(this).fadeOut(2000);
+                  
+  });
+});
